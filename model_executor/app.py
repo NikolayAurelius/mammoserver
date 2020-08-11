8ec7809ccfc8b246e154f65f0c256cac5b9eead3
NikolayAurelius <marcusaureliuswork@gmail.com>
2020-08-11 14:08:52 +0300

    Add uploading file

--- model_executor/app.py
+++ model_executor/app.py
@@ -1,13 +1,58 @@
-from flask import Flask
+from flask import Flask, request, redirect, flash, url_for
 from models import model_1, model_2, model_3, model_4
+import os
+from datetime import datetime
+
+WORKDIR = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = f'{WORKDIR}/uploads'
+ALLOWED_EXTENSIONS = {'bin', }
+
+if not os.path.exists(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 
 models = [model_1, model_2, model_3, model_4]
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return str([elem.name for elem in models])
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = f'{file.filename}_{datetime.now().strftime("%d/%m/%Y %H:%M:%S")}'
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('uploaded_file',
+                                    filename=filename))
+    return f'''
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <input type=file name=file>
+      <input type=submit value=Upload>
+    </form>
+    
+    <h1>Models: {str([elem.name for elem in models])}</h1>
+    '''
 
 
 if __name__ == '__main__':
