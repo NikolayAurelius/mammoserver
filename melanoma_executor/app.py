ec04573cc7da5efd1bb2e93d25db7e8e87d33ca5
tyugv <milsocol@gmail.com>
2020-08-13 19:04:33 +0300

    fix configs

--- melanoma_executor/app.py
+++ melanoma_executor/app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, redirect, flash, url_for, jsonify, render_template
+from flask import Flask, request, redirect, flash, url_for, render_template
 from models import model, runModel
 import os
 from datetime import datetime
 import numpy as np
 
 WORKDIR = os.path.dirname(os.path.abspath(__file__))
-UPLOAD_FOLDER = f'{WORKDIR}/uploads'
-ALLOWED_EXTENSIONS = {'jpg', }
+UPLOAD_FOLDER = f'{WORKDIR}/static/uploads'
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
@@ -19,17 +19,22 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
+@app.route('/')
+def index():
+    return redirect(url_for('upload_file'))
+
+
 @app.route('/melanoma', methods=['GET', 'POST'])
 def upload_file():
 
     status = None
+    showimg = None
 
     if request.method == 'POST':
 
         # check if the post request has the file part
         if 'file' not in request.files:
             status = 'Error: No file part'
-            #return jsonify({'error': True, 'error_type': 'No file part'})
 
         file = request.files['file']
 
@@ -37,15 +42,14 @@ def upload_file():
             print(file.filename)
             filename = f'{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.jpg'
             file.save(os.path.join(UPLOAD_FOLDER, filename))
-
+            showimg = filename
             diagns = runModel(os.path.join(UPLOAD_FOLDER, filename), model)
             result = diagns
-            status = f'Your result: {str(result[0][0])}'
-            #return jsonify({'error': False, 'result': str(result[0][0])})
+            status = f'Your result: {str(int(100 * result[0][0]))}%'
         else:
-            status = 'Error: may be you tried to download not img file'
+            status = 'File should be .jpg, .jpeg or .png'
 
-    return render_template('main_page.html', status = status)
+    return render_template('main_page.html', status = status, showimg = showimg)
 
 
 if __name__ == '__main__':
