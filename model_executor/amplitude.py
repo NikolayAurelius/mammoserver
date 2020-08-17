c72adb271fcbae092d1692e61051301ba5fd75db
NikolayAurelius <marcusaureliuswork@gmail.com>
2020-08-17 12:32:14 +0300

    With max min amplitude and model execution

--- model_executor/amplitude.py
+++ model_executor/amplitude.py
@@ -1,27 +1,27 @@
 import numpy as np
-from scipy.optimize import minimize_scalar
+import torch
 
 
-def grad(weights: np.array, t: float, v: float):
-    # TODO: use Cython
-    a = weights[0]
-    e = weights[1]
-    b = weights[2]
-
-    w = weights[3]
-
-    undersin = e + t * w
-
-    sin_value = np.sin(w * t + e)
-    cos_value = np.cos(w * t + e)
-
-    dlda = (a * sin_value + b - v) * sin_value
-    dlde = a * (a * sin_value + b - v) * cos_value
-    dldb = a * sin_value + b - v
-
-    dldw = a * t * (a * sin_value + b - v) * cos_value
-
-    return np.array([dlda, dlde, dldb, dldw])
+# def grad(weights: np.array, t: float, v: float):
+#     # TODO: use Cython
+#     a = weights[0]
+#     e = weights[1]
+#     b = weights[2]
+#
+#     w = weights[3]
+#
+#     undersin = e + t * w
+#
+#     sin_value = np.sin(w * t + e)
+#     cos_value = np.cos(w * t + e)
+#
+#     dlda = (a * sin_value + b - v) * sin_value
+#     dlde = a * (a * sin_value + b - v) * cos_value
+#     dldb = a * sin_value + b - v
+#
+#     dldw = a * t * (a * sin_value + b - v) * cos_value
+#
+#     return np.array([dlda, dlde, dldb, dldw])
 
 
 class Amplitude:
@@ -29,58 +29,13 @@ class Amplitude:
         self.raw_measurement = raw_measurement
 
 
-def raw_measurement_to_amplitude(raw_measurement: np.array = np.zeros((18, 18, 18, 18, 80))):
-    # Приравнивать туда-обратно?
-    weights = np.random.uniform()
-
-    pass
+def max_min_approximate_aplitude(meas):
+    meas = np.sort(meas, axis=4)
+    return np.abs(meas[:, :, :, :, -1] - meas[:, :, :, :, 0]) / 2
 
 
-
-def real_func(t):
-    return 5.0 * np.sin(10.0 * t + 0.5) + 0.1
-
-
-def loss(a, w, t, e, b, v):
-    return (a * np.sin(w * t + e) + b - v) ** 2
-
-dataset = [(t, real_func(t)) for t in range(80)]
-
-samples = 2
-
-w = 10.0
-x0 = np.random.uniform(0.1, 3.0, 3)
-x0[2] = 0.1
-print(x0)
-for i in range(1000):
-    g = np.zeros((3, ))
-    for j in range(samples):
-        g += grad(x0[0], w, dataset[j][0], x0[1], x0[2], dataset[j][1])
-
-    g[2] = 0.0
-    dx = g / np.linalg.norm(g)
-
-    last_loss = 1e6
-    for step in range(100000):
-        my_dx = (step / 10000) * dx
-        my_x = x0 - my_dx
-
-        my_loss = 0
-        for j in range(samples):
-            my_loss += loss(my_x[0], w, dataset[j][0], my_x[1], my_x[2], dataset[j][1])
-        if my_loss > last_loss:
-            print(my_loss)
-            break
-        last_loss = my_loss
-    x1 = x0 - my_dx
-
-    print(x1)
-    x0 = x1
-    # break
-
-
-
-
-
-
+def meas_to_x(meas: np.array = np.zeros((18, 18, 18, 18, 80))):
+    amplitude = max_min_approximate_aplitude(meas)
+    x = np.expand_dims(np.expand_dims(amplitude, axis=0), axis=0)
+    return x
 
--- model_executor/app.py
+++ model_executor/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, redirect, flash, url_for, jsonify
-from models import model_1, model_2, model_3, model_4
+from models import model_1, model_2, model_3, model_4, bad, stranger
 import os
 from datetime import datetime
 import numpy as np
+from .mammo_packets import read_from_file_binary, parse_mammograph_raw_data, parse_uncompressed_mammograph_packets
+from .amplitude import meas_to_x
 
 WORKDIR = os.path.dirname(os.path.abspath(__file__))
 UPLOAD_FOLDER = f'{WORKDIR}/uploads'
-ALLOWED_EXTENSIONS = {'bin', }
+ALLOWED_EXTENSIONS = {'bin', 'biz'}
 
 if not os.path.exists(UPLOAD_FOLDER):
     os.mkdir(UPLOAD_FOLDER)
@@ -39,11 +41,26 @@ def upload_file():
         #     return redirect(request.url)
         if file and allowed_file(file.filename):
             print(file.filename)
-            filename = f'{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.bin'
-            file.save(os.path.join(UPLOAD_FOLDER, filename))
-            result = {'bad': np.random.uniform(), 'stranger': np.random.uniform()}
+            extention = file.filename.rsplit('.', 1)[1].lower()
+            filename = f'{datetime.now().strftime("%d_%m_%Y_%H_%M_%S")}.{extention}'
+            path = os.path.join(UPLOAD_FOLDER, filename)
+            file.save(path)
+            parser = parse_mammograph_raw_data
+
+            # TODO: Из-за того, что утилита сохраняет всё под одним файлом можем работать только с одним воркером
+            if extention == 'biz':
+                parser = parse_uncompressed_mammograph_packets
+                raise NotImplementedError()
+
+            data = read_from_file_binary(path)
+            arr = parser(data)
+            x = meas_to_x(arr)
+
+            result = {'bad': bad(x)}
             for model in models:
-                result[model.name] = np.random.uniform()
+                result[model.name] = model(x)
+
+            result['stranger'] = stranger(result['torch_model_3'])
             return jsonify({'error': False, 'result': result})
         else:
             return jsonify({'error': True})
